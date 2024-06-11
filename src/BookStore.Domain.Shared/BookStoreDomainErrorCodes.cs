6f6169766f95b641be3ce574fd0e4aca7e5c05a3
Dennis Hidalgo <124583417+Dennis-Hidalgo@users.noreply.github.com>
2024-06-11 12:53:29 +0200

    Add files via upload

---
+++ src/BookStore.Domain.Shared/BookStoreDomainErrorCodes.cs
@@ -0,0 +1,7 @@
+﻿namespace BookStore;
+
+public static class BookStoreDomainErrorCodes
+{
+    public const string AuthorAlreadyExists = "BookStore:00001";
+    public const string NoStockAvailable = "BookStore:00002";
+}
---
+++ src/BookStore.Domain.Shared/Markers/MarkerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Markers
+{
+    public enum MarkerType
+    {
+        undefined,
+        shop,
+        restaurant,
+        gym
+    }
+}
